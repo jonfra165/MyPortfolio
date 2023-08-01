b27e84c3ff8a06fd8dd10f64c24326147fd5e760
jonfra165 <76876808+jonfra165@users.noreply.github.com>
2023-08-01 16:32:58 +0200

    Update AboutMe.js

--- my-portfolio/src/components/AboutMe.js
+++ my-portfolio/src/components/AboutMe.js
@@ -11,7 +11,7 @@ const AboutMe = () => {
         {/* Första boxen med text */}
         <Grid item xs={12} sm={6}>
           <Box>
-            <p style=>
+            <p style={{ width= 50%}}>
             I recently completed my studies in Computer and Information Science after three years of hard work, graduating in June 2023. Throughout my education, I gained hands-on experience in both backend and frontend development, with expertise in a wide range of technologies, including Bootstrap, Angular, React, Vue, Node, React Native, Django, and Flask. My optimism, energy, motivation, and loyalty are characteristics that define me both personally and professionally. With a strong foundation in object-oriented programming, interaction design, UX, and usability principles, I'm confident in my ability to create effective, sustainable and engaging digital experiences. I am a quick learner and always maintain a positive and flexible attitude. Whether it's expanding my existing knowledge or diving into unknown technologies, I'm always motivated to improve.
 
             </p>
@@ -23,4 +23,3 @@ const AboutMe = () => {
 };
 
 export default AboutMe;
-??
